90b8865618166c091d4f8b6aaca727112f8fe4bf
takjinwu <105762293+takjinwu@users.noreply.github.com>
2025-05-07 14:27:10 +0900

    Add files via upload

---
+++ 2_1강/App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello</Text>
+      <StatusBar style="dark" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 28,
+    color: "black",
+  },
+});
