1158285d8b472344f542b597df8446d1f808207f
takjinwu <105762293+takjinwu@users.noreply.github.com>
2025-05-07 14:44:29 +0900

    Update App.js

--- 2_4강/App.js
+++ 2_4강/App.js
@@ -1,6 +1,12 @@
+import React from "react";
 import { View } from "react-native";
 
+export default function App() {
+  return (
     <View style={{ flex: 1, flexDirection: "row" }}>
     <View style={{ flex: 1, backgroundColor: "tomato" }}></View>
     <View style={{ flex: 1, backgroundColor: "teal" }}></View>
     <View style={{ flex: 1, backgroundColor: "orange" }}></View>
+    </View>
+  );
+}
